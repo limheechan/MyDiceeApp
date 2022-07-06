8b7e647d79b76cd18ddafcaa6dc486261ad980d6
limheechan <decodeode@gmail.com>
2022-07-06 19:23:47 +0900

    Initial Commit

---
+++ MyDiceeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDiceeApp
+//
+//  Created by 임희찬 on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
