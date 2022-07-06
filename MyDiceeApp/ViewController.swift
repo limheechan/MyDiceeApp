8cfc287f5091dd620ad05915942680351001329b
limheechan <decodeode@gmail.com>
2022-07-06 22:48:30 +0900

    imageView alpha 값 변경 및 주사위 이미지 2 연결

--- MyDiceeApp/ViewController.swift
+++ MyDiceeApp/ViewController.swift
@@ -11,12 +11,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // #imageLiteral()   ⬇️
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
     
 
