657375b4bdd39a2a46225b2b056a41b8e3275c7c
limheechan <decodeode@gmail.com>
2022-07-07 00:33:56 +0900

    total point 와 point reset button 추가

--- MyDiceeApp/ViewController.swift
+++ MyDiceeApp/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    @IBOutlet weak var point: UILabel!
     
+    var pointNumb = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,10 +23,21 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //변할 필요 없어 상수 값으로 지정
         let diceNumber = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        let dice1 = Int.random(in: 0...5)
         
-        diceImageView1.image = diceNumber[Int.random(in: 0...5)]
-        diceImageView2.image = diceNumber[Int.random(in: 0...5)]
+        let dice2 = Int.random(in: 0...5)
+        
+        pointNumb += (dice1+1)+(dice2+1)
+        point.text = String(pointNumb)
+        diceImageView1.image = diceNumber[dice1]
+        diceImageView2.image = diceNumber[dice2]
+        
+       
+        
     }
     
+    @IBAction func resetButton(_ sender: UIButton) {
+        point.text = String(0)
+    }
 }
 
